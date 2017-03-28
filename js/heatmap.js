e8d23cb79af27f86dffa09ac04cfa2103ce155c0
Coy Sanders <coymeetsworld@gmail.com>
2017-03-27 20:43:01 -0700

    Refactored with ES6

--- js/heatmap.js
+++ js/heatmap.js
@@ -1,32 +1,34 @@
 
-  var monthNames = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
-  var heatRange = [0, 2.7, 3.9, 5, 6.1, 7.2, 8.3, 9.4, 10.5, 11.6];
+  const MONTH_NAMES = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
+  const HEAT_RANGE = [0, 2.7, 3.9, 5, 6.1, 7.2, 8.3, 9.4, 10.5, 11.6];
   
-  var margin = {top: 20, right: 30, bottom: 30, left: 40}
-  var chartWidth = 1270 - margin.left - margin.right;
-  var chartHeight = 650 - margin.top - margin.bottom;
+  const MARGIN = {top: 20, right: 30, bottom: 30, left: 40}
+  const CHART_WIDTH = 1270 - MARGIN.left - MARGIN.right;
+  const CHART_HEIGHT = 650 - MARGIN.top - MARGIN.bottom;
 
-  var x = d3.scaleLinear().range([0, chartWidth]);
-  var y = d3.scaleLinear().domain([12,0]).range([chartHeight, 0]);
+  let x = d3.scaleLinear().range([0, CHART_WIDTH]);
+  let y = d3.scaleLinear().domain([12,0]).range([CHART_HEIGHT, 0]);
 
-  var chart = d3.select('#chart')
-                .attr("width", chartWidth+margin.left+margin.right)
-                .attr("height", chartHeight+margin.top+margin.bottom)
+  let chart = d3.select("#chart")
+                .attr("width", CHART_WIDTH + MARGIN.left + MARGIN.right)
+                .attr("height", CHART_HEIGHT + MARGIN.top + MARGIN.bottom)
                 .append("g")
-                .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
+                .attr("transform", `translate(${MARGIN.left},${MARGIN.top})`);
 
 
   /* 
     Renders the x-axis of the graph with label.
   */
-  function buildXAxis(x, minYear, maxYear) {
+  const buildXAxis = (x, minYear, maxYear) => {
 
     chart.append("g")
-         .attr("transform", "translate(0," + chartHeight + ")")
-         .call(d3.axisBottom(x).tickArguments([(maxYear - minYear)/10, "d"]));
+         .attr("transform", `translate(0,${CHART_HEIGHT})`)
+         .call(d3.axisBottom(x)
+                 .tickArguments([(maxYear - minYear)/10, "d"]));
 
     chart.append("text")
-         .attr("transform", "translate(" + (chartWidth/2) + "," + (chartHeight+50) + ")")
+         .attr("transform", "translate(" + (CHART_WIDTH/2) + "," + (CHART_HEIGHT+50) + ")")
+         .attr("transform", `translate(${CHART_WIDTH/2},${CHART_HEIGHT+50})`)
          .attr("class", "axis-label")
          .text("Years");
   }
@@ -35,20 +37,19 @@
   /*
     Renders the y-axis of the graph with label.
   */
-  function buildYAxis(y) {
+  const buildYAxis = (y) => {
+    
     chart.append("g")
          .attr("transform", "translate(0,0)")
          .call(d3.axisLeft(y)
-                  .ticks(16)
+                 .ticks(16)
                  .tickSize(16,0)
-                 .tickFormat(function(d) {
-                   return monthNames[d];
-                 }));
+                 .tickFormat(d => MONTH_NAMES[d]));
 
     chart.append("text")
          .attr("transform", "rotate(-90)")
          .attr("y", -125)
-         .attr("x", chartHeight/-2+50)
+         .attr("x", CHART_HEIGHT/-2+50)
          .attr("class", "axis-label")
          .attr("dy", "2em")
          .text("Months");
@@ -58,39 +59,39 @@
   /* 
     Renders the legend for the map.
   */
-  function buildLegend() {
+  const buildLegend = () => {
 
-    var legendElemHeight = 20;
-    var legendElemWidth = 55;
-    var legendElemTextMarginTop = 35;
-    var legendElemTextMarginLeft = 5;
+    const LEGEND_ELEM_HEIGHT = 20;
+    const LEGEND_ELEM_WIDTH = 55;
+    const LEGEND_ELEM_TEXT_MARGIN_TOP = 35;
+    const LEGEND_ELEM_TEXT_MARGIN_LEFT = 5;
     
-    var legend = chart.append("g")
-    .attr("transform", "translate(650,75)")
-    .selectAll(".legend")
-    .data(heatRange).enter();
+    let legend = chart.append("g")
+                      .attr("transform", "translate(650,75)")
+                      .selectAll(".legend")
+                      .data(HEAT_RANGE).enter();
     
     legend.append("rect")
-           .attr("x", function(d, i) { return legendElemWidth*i; })
-           .attr("y", chartHeight)
-           .attr("width", legendElemWidth)
-           .attr("height", legendElemHeight)
-            .attr("class", function(d) { return getGridColor(d); });
+          .attr("x", (d, i) => LEGEND_ELEM_WIDTH*i)
+          .attr("y", CHART_HEIGHT)
+          .attr("width", LEGEND_ELEM_WIDTH)
+          .attr("height", LEGEND_ELEM_HEIGHT)
+          .attr("class", d => getGridColor(d));
 
     legend.append("text")
-         .text(function(d) { return ">= " + d; })
+         .text(d => ">= " + d)
          .attr("class", "legend-text")
-         .attr("x", function(d, i) { return (legendElemWidth*i)+legendElemTextMarginLeft; })
-         .attr("y", chartHeight+legendElemTextMarginTop);
+         .attr("x", (d, i) => (LEGEND_ELEM_WIDTH * i) + LEGEND_ELEM_TEXT_MARGIN_LEFT)
+         .attr("y", CHART_HEIGHT + LEGEND_ELEM_TEXT_MARGIN_TOP);
   }
 
 
   /* 
     Returns the CSS class used to fill out a cell on the heatmap. CSS class depends on temp variable passed.
   */
-  function getGridColor(temp) {
+  const getGridColor = (temp) => {
 
-    if (temp <= 0)           { return "gradient11"; }
+    if (temp <= 0)          { return "gradient11"; }
     else if (temp <= 2.7)   { return "gradient10"; }
     else if (temp <= 3.9)   { return "gradient9"; }
     else if (temp <= 5)     { return "gradient8"; }
@@ -98,48 +99,48 @@
     else if (temp <= 7.2)   { return "gradient6"; }
     else if (temp <= 8.3)   { return "gradient5"; }
     else if (temp <= 9.4)   { return "gradient4"; }
-    else if (temp <= 10.5)   { return "gradient3"; }
-    else if (temp <= 11.6)   { return "gradient2"; }
-    else                     { return "gradient1"; }
+    else if (temp <= 10.5)  { return "gradient3"; }
+    else if (temp <= 11.6)  { return "gradient2"; }
+    else                    { return "gradient1"; }
   }
+
 
   d3.json('https://raw.githubusercontent.com/FreeCodeCamp/ProjectReferenceData/master/global-temperature.json', function(error, heatData) {
     if (error) throw error;
 
-    var minYear = d3.min(heatData.monthlyVariance, function(d) { return d.year; });
-    var maxYear = d3.max(heatData.monthlyVariance, function(d) { return d.year; });
+    var minYear = d3.min(heatData.monthlyVariance, d => d.year);
+    var maxYear = d3.max(heatData.monthlyVariance, d => d.year);
     x.domain([minYear, maxYear+1]);
 
-    console.log("Number of cells: " + heatData.monthlyVariance.length);
     var baseTemp = heatData.baseTemperature;
     var numYears = Math.ceil(heatData.monthlyVariance.length/12);
     console.log("Years: " + numYears);
-    var cellWidth = (chartWidth)/numYears;
-    var cellHeight = (chartHeight)/12;
-    console.log("chartWidth: " + (chartWidth));
+    var cellWidth = (CHART_WIDTH)/numYears;
+    var cellHeight = (CHART_HEIGHT)/12;
+    console.log("CHART_WIDTH: " + (CHART_WIDTH));
     console.log("cell width: "+ cellWidth);
-    console.log("chart height: " + (chartHeight));
+    console.log("chart height: " + (CHART_HEIGHT));
     console.log("cell height: "+ cellHeight);
     
 
     var cards = chart.selectAll("g")
                       .data(heatData.monthlyVariance)
                       .enter().append("rect")
-                      .attr("title", function(d) { return `${d.year} ${d.month}`; })
-                      .attr("x", function(d) { return x(d.year); })
-                      .attr("y", function(d) { return y(d.month-1); })
+                      .attr("title", d => `${d.year} ${d.month}`)
+                      .attr("x", d => x(d.year))
+                      .attr("y", d => y(d.month-1))
                       .attr("height", cellHeight)
-                      .attr("class", function(d) { return getGridColor(baseTemp + d.variance); })
+                      .attr("class", d => getGridColor(baseTemp + d.variance))
                       .attr("width", cellWidth)
                       .on("mouseover", function() {
                         console.log(d3.select(this).datum());
-                        var cellData = d3.select(this).datum();
+                        let cellData = d3.select(this).datum();
                         d3.select(this).attr('class', 'highlight');
 
-                        var xPos = parseFloat(d3.select(this).attr("x"));
-                        var yPos = parseFloat(d3.select(this).attr("y"));
-                        var width = parseFloat(d3.select(this).attr("width"));
-                        var height = parseFloat(d3.select(this).attr("height"));
+                        let xPos = parseFloat(d3.select(this).attr("x"));
+                        let yPos = parseFloat(d3.select(this).attr("y"));
+                        let width = parseFloat(d3.select(this).attr("width"));
+                        let height = parseFloat(d3.select(this).attr("height"));
                         console.log("x: " + xPos + " y: " + yPos + " width: " + width + " height: " + height);
 
                         chart.append('rect')
@@ -154,27 +155,27 @@
                             'fill': 'rgba(0,0,0,0.9)',
                             'border': '2px solid #fff'
                           });
+                          
                         chart.append('text')
                           .attr('class','tip')
                           .html(
-                            "<tspan x=" + (xPos-80) + " y=" + (yPos-30) + " class=\"tooltip-date\">" + cellData.year + " - " + monthNames[cellData.month-1] + "</tspan>"
-                            + "<tspan x=" + (xPos-80) + " y=" + (yPos-5) + " class=\"tooltip-temperature\">" 
-                              + (baseTemp + cellData.variance).toFixed(3) + "&deg;C&nbsp;&nbsp;( " + 
-                              + cellData.variance.toFixed(3)
+                            `<tspan x=${xPos-80} y=${yPos-30} class="tooltip-date"> ${cellData.year} - ${MONTH_NAMES[cellData.month-1]}</tspan>`
+                            + `<tspan x=${xPos-80} y=${yPos-5} class="tooltip-temperature">` 
+                            + (baseTemp + cellData.variance).toFixed(3) + "&deg;C&nbsp;&nbsp;( "
+                            + cellData.variance.toFixed(3)
                             + " )</tspan>")
                           .attr('x', xPos - 85)
                           .attr('y', yPos - 30)
                           .style({
                             fill: "#fff",
-                            'font-weight': 'bold',
-                            'font-size': '1.2em'
+                            'font-size': '1.2em',
+                            'font-weight': 'bold'
                           });
-
                       })
-                     .on('mouseout', function(d) {
-                       chart.selectAll('.tip').remove();
-                       d3.select(this).attr('class', function(d) { return getGridColor(baseTemp + d.variance); });
-                     });
+                      .on('mouseout', function(d) {
+                        chart.selectAll('.tip').remove();
+                        d3.select(this).attr('class', (d) => getGridColor(baseTemp + d.variance));
+                      });
 
     buildXAxis(x, minYear, maxYear);
     buildYAxis(y);
